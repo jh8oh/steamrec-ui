f18cc568f59032bb4af0af09000e2f73186b2b39
jh8oh <ohjiho.ca@gmail.com>
2022-11-29 04:28:26 +0700

    Added rating functionality

--- src/views/components/GameView.vue
+++ src/views/components/GameView.vue
@@ -5,14 +5,15 @@
     />
     <div class="game-view-rating-peek" />
     <div class="game-view-rating-system">
-      <img :src="getThumbsDownSrc()" />
-      <img :src="getThumbsUpSrc()" />
+      <img :src="getThumbsDownSrc()" @click="onThumbsDownClicked()" />
+      <img :src="getThumbsUpSrc()" @click="onThumbsUpClicked()" />
     </div>
   </figure>
 </template>
 
 <script lang="ts">
 import { Vue } from "vue-class-component";
+import axios from "axios";
 import { Game } from "@/models/game";
 
 import thumbsUpUnclicked from "@/assets/thumbs-up-unclicked.svg";
@@ -25,6 +26,31 @@ class Prop {
 }
 
 export default class GameView extends Vue.with(Prop) {
+  private onThumbsUpClicked() {
+    if (this.game.rating == 2) {
+      this.rate(0);
+    } else {
+      this.rate(2);
+    }
+  }
+
+  private onThumbsDownClicked() {
+    if (this.game.rating == 1) {
+      this.rate(0);
+    } else {
+      this.rate(1);
+    }
+  }
+
+  private rate(rating: number) {
+    this.game.rating = rating;
+
+    axios.post("http://localhost:8080/data/ratings/rate", {
+      gameId: this.game.id,
+      rating: rating,
+    });
+  }
+
   private getThumbsUpSrc() {
     if (this.game.rating == 2) {
       return thumbsUpClicked;
