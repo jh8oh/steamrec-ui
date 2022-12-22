15232c86951f3502c195a65cb526732095958609
jh8oh <ohjiho.ca@gmail.com>
2022-12-23 05:40:23 +0700

    Filter CSS

--- src/views/Rec.vue
+++ src/views/Rec.vue
@@ -4,79 +4,112 @@
       You have not rated any games. Return to the <strong>Rate</strong> tab and
       rate games to get recommendations.
     </div>
-    <div v-if="!hasNoRatedGames">
-      <ul id="rec-filter">
-        <li>
-          <span>Type</span>
-          <ul>
-            <li v-for="type in allTypes" :key="type">
-              <input
-                type="checkbox"
-                :id="type"
-                :value="type.toLowerCase()"
-                v-model.lazy="filter.type"
-              />
-              <label :for="type">{{ type }}</label>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <label for="released">Released</label>
-          <input type="checkbox" id="released" v-model.lazy="filter.released" />
-        </li>
-        <li>
-          <label for="recommendations">Recommendations</label>
-          <input
-            type="range"
-            id="recommendations"
-            min="0"
-            max="10"
-            v-model.lazy="recommendations"
-          />
-        </li>
-        <li>
-          <span>Metacritic Score</span>
-          <input
-            type="radio"
-            id="acclaim"
-            value="90"
-            v-model.lazy="filter.metacritic"
-          />
-          <label for="acclaim">Universal Acclaim</label>
-          <input
-            type="radio"
-            id="favorable"
-            value="75"
-            v-model.lazy="filter.metacritic"
-          />
-          <label for="favorable">Generally Favorable Reviews</label>
-          <input
-            type="radio"
-            id="mixed"
-            value="50"
-            v-model.lazy="filter.metacritic"
-          />
-          <label for="mixed">Mixed or Average Reviews</label>
-          <input
-            type="radio"
-            id="unfavorable"
-            value="20"
-            v-model.lazy="filter.metacritic"
-          />
-          <label>Generally Unfavorable Reviews</label>
-          <input
-            type="radio"
-            id="disliked"
-            value="0"
-            v-model.lazy="filter.metacritic"
-          />
-          <label>Overwhelming Dislike</label>
-        </li>
-        <li>
-          <label for="adult">Include adult games</label>
-          <input type="checkbox" id="adult" v-model.lazy="filter.adult" />
-        </li>
-      </ul>
+    <div id="rated" v-if="!hasNoRatedGames">
+      <aside id="rec-filter">
+        <ul>
+          <li>
+            <span class="filter-title">Type</span>
+            <ul class="filter-types-ul">
+              <li v-for="type in allTypes" :key="type">
+                <input
+                  type="checkbox"
+                  :id="type"
+                  :value="type.toLowerCase()"
+                  v-model.lazy="filter.type"
+                />
+                <label :for="type" class="filter-radio-label">{{ type }}</label>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <input
+              type="checkbox"
+              id="released"
+              v-model.lazy="filter.released"
+            />
+            <label for="released" class="filter-title">Released</label>
+          </li>
+          <li>
+            <label for="recommendations" class="filter-title">
+              Recommendations
+            </label>
+            <span id="recommendations-value">
+              &gt;{{ filter.recommendations }}
+            </span>
+            <input
+              type="range"
+              id="recommendations"
+              min="0"
+              max="10"
+              v-model.lazy="recommendations"
+            />
+          </li>
+          <li>
+            <span class="filter-title">Metacritic Score</span>
+            <ul>
+              <li>
+                <input
+                  type="radio"
+                  id="acclaim"
+                  value="90"
+                  v-model.lazy="filter.metacritic"
+                />
+                <label for="acclaim" class="filter-radio-label">
+                  Universal&nbsp;Acclaim
+                </label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  id="favorable"
+                  value="75"
+                  v-model.lazy="filter.metacritic"
+                />
+                <label for="favorable" class="filter-radio-label">
+                  Generally&nbsp;Favorable&nbsp;Reviews
+                </label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  id="mixed"
+                  value="50"
+                  v-model.lazy="filter.metacritic"
+                />
+                <label for="mixed" class="filter-radio-label">
+                  Mixed&nbsp;or&nbsp;Average&nbsp;Reviews
+                </label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  id="unfavorable"
+                  value="20"
+                  v-model.lazy="filter.metacritic"
+                />
+                <label for="unfavorable" class="filter-radio-label">
+                  Generally&nbsp;Unfavorable&nbsp;Reviews
+                </label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  id="disliked"
+                  value="0"
+                  v-model.lazy="filter.metacritic"
+                />
+                <label for="disliked" class="filter-radio-label">
+                  Overwhelming&nbsp;Dislike
+                </label>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <input type="checkbox" id="adult" v-model.lazy="filter.adult" />
+            <label for="adult" class="filter-title">Include adult games</label>
+          </li>
+        </ul>
+      </aside>
       <div>{{ filter }}</div>
     </div>
   </div>
